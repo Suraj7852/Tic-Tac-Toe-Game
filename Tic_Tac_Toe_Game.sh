d384df4a59b84bdac09d4483628434dfec5dcbd2
Suraj7852 <surajpj7852@gmail.com>
2019-11-16 17:37:25 +0530

    ADDED: value added into array

--- Tic_Tac_Toe_Game.sh
+++ Tic_Tac_Toe_Game.sh
@@ -1,3 +1,11 @@
 #!/bin/bash -x
 
 echo "--------------------------Tic Tac Toe Game-----------------------"
+
+declare -a PlayingBoard
+for (( counter=1; counter<=9; counter++ ))
+do
+	PlayingBoard[$counter]=$counter;
+done
+
+echo ${PlayingBoard[@]}
