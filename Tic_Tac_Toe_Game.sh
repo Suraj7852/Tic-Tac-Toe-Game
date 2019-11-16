b09efc8edb20d0a3c46f9fc61075d2185a2374dd
Suraj7852 <surajpj7852@gmail.com>
2019-11-16 18:39:00 +0530

    ADDED: Displaying the Playing Board

--- Tic_Tac_Toe_Game.sh
+++ Tic_Tac_Toe_Game.sh
@@ -21,5 +21,18 @@ function TOSS(){
 	echo $choiceToPlayFirst
 }
 
+function displayBoard(){
+	echo "-----------------------"
+	echo "||  ${PlayingBoard[1]}  ||  ${PlayingBoard[2]}  ||  ${PlayingBoard[3]}  ||"
+	echo "-----------------------"
+	echo "||     ||     ||     ||"
+	echo "||  ${PlayingBoard[4]}  ||  ${PlayingBoard[5]}  ||  ${PlayingBoard[6]}  ||"
+	echo "-----------------------"
+	echo "||     ||     ||     ||"
+	echo "||  ${PlayingBoard[7]}  ||  ${PlayingBoard[8]}  ||  ${PlayingBoard[9]}  ||"
+	echo "-----------------------"
+}
+
 echo ${PlayingBoard[@]}
-echo $( TOSS )
+TOSS 
+displayBoard
