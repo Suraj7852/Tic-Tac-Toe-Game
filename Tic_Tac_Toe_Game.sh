6baa77adef39bdaa9d36144a1a699c09f1e7b779
Suraj7852 <surajpj7852@gmail.com>
2019-11-16 18:08:53 +0530

    ADDED: Toss to choose who will play first

--- Tic_Tac_Toe_Game.sh
+++ Tic_Tac_Toe_Game.sh
@@ -10,5 +10,16 @@ done
 
 computer="X";
 player="O";
+function TOSS(){
+	toss=$((RANDOM%2))
+	if [ $toss -eq 0 ]
+	then
+		choiceToPlayFirst=$computer
+	else
+		choiceToPlayFirst=$player
+	fi	
+	echo $choiceToPlayFirst
+}
 
 echo ${PlayingBoard[@]}
+echo $( TOSS )
