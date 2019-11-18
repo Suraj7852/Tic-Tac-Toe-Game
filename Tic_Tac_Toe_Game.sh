e95da708d3f76214de566d7f7f98d568e4ca2f44
Suraj7852 <surajpj7852@gmail.com>
2019-11-18 12:38:17 +0530

    ADDED: functionality for row win coloumn win and diagonal win

--- Tic_Tac_Toe_Game.sh
+++ Tic_Tac_Toe_Game.sh
@@ -18,7 +18,7 @@ function TOSS(){
 		choiceToPlayFirst=$COMPUTER
 	else
 		choiceToPlayFirst=$PLAYER
-	fi	
+	fi
 	echo $choiceToPlayFirst
 }
 
@@ -45,11 +45,75 @@ function findReplace(){
 	done
 }
 
+function rowWinnerCheck(){
+	local row=0;
+	local winnerRecieved=false;
+	for (( count=1; count<=3; count++ ))
+	do
+		row=$(( $row+1 ))
+		if [[ ${PlayingBoard[$row]} == ${PlayingBoard[$row+1]} ]] && [[ ${PlayingBoard[$row+1]} == ${PlayingBoard[$row+2]} ]]
+		then
+			winnerRecieved=true;
+			break;
+		else
+			row=$(( $row+2 ))
+		fi
+	done
+	echo $winnerRecieved
+}
 
+function coloumnWinnerCheck(){
+	local coloumn=0;
+	local winnerRecieved=false;
+	for (( count=1; count<=3; count++ ))
+	do
+		coloumn=$(( $coloumn+1 ))
+		if [[ ${PlayingBoard[$coloumn]} == ${PlayingBoard[$coloumn+3]} ]] && [[ ${PlayingBoard[$coloumn+3]} == ${PlayingBoard[$coloumn+6]} ]]
+		then
+			winnerRecieved=true;
+			break;
+		fi
+	done
+	echo $winnerRecieved
+}
+
+function diagonalWinnerCheck(){
+	local winnerRecieved=false
+	for (( count=1; count<=3; count++ ))
+	do
+		if [[ ${PlayingBoard[$count]} == ${PlayingBoard[$count+4]} ]] && [[ ${PlayingBoard[$count+4]} == ${PlayingBoard[$count+8]} ]]
+		then
+			winnerRecieved=true;
+			break;
+		elif [[ ${PlayingBoard[$count+2]} == ${PlayingBoard[$count+4]} ]] && [[ ${PlayingBoard[$count+4]} == ${PlayingBoard[$count+6]} ]]
+		then
+			winnerRecieved=true;
+			break;
+		fi
+	done
+	echo $winnerRecieved;
+}
+
+function winnerCheck() {
+	local row=$( rowWinnerCheck )
+	local coloumn=$( coloumnWinnerCheck )
+	local diagonal=$( diagonalWinnerCheck )
+	local winnerRecieved=false;
+	if [[ $row == true ]] || [[ $coloumn == true ]] || [[ $diagonal == true ]]
+	then
+		winnerRecieved=true;
+	fi
+	echo $winnerRecieved;
+}
 displayBoard
 while [ true ]
 do
 	read -p "Enter your choice" replace
 	findReplace $replace
 	displayBoard
+	winner=$( winnerCheck )
+	if [ $winner == true ]
+	then
+		break;
+	fi
 done
