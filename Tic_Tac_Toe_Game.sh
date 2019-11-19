94e8814ceee4ae8d2884466cda72821757332af2
Suraj7852 <surajpj7852@gmail.com>
2019-11-19 09:49:27 +0530

    ADDED: functionality for if can win then play that move

--- Tic_Tac_Toe_Game.sh
+++ Tic_Tac_Toe_Game.sh
@@ -4,6 +4,7 @@ echo "--------------------------Tic Tac Toe Game-----------------------"
 
 COMPUTER="X";
 PLAYER="O";
+moveCount=0;
 declare -a PlayingBoard
 
 for (( counter=1; counter<=9; counter++ ))
@@ -41,6 +42,17 @@ function findReplace(){
 		if [[ ${PlayingBoard[i]} == $replace ]]
 		then
 			PlayingBoard[$i]=$PLAYER
+		fi
+	done
+}
+
+function findReplaceCom(){
+	replace=$1;
+	for (( i=0;i<=${#PlayingBoard[@]}; i++ ))
+	do
+		if [[ ${PlayingBoard[i]} == $replace ]]
+		then
+			PlayingBoard[$i]=$COMPUTER
 		fi
 	done
 }
@@ -105,15 +117,107 @@ function winnerCheck() {
 	fi
 	echo $winnerRecieved;
 }
+
+function rowCanWin(){
+	local row=0;
+	for (( count=1; count<=3; count++ ))
+	do
+		row=$(( $row+1 ))
+		if [[ ${PlayingBoard[$row]} == ${PlayingBoard[$row+1]} ]] || [[ ${PlayingBoard[$row+1]} == ${PlayingBoard[$row+2]} ]] || [[ ${PlayingBoard[$row+2]} == ${PlayingBoard[$row]} ]]
+		then
+			echo $row
+			for (( i=$row; i<=$row+2; i++ ))
+			do
+				if [[ ${PlayingBoard[$i]} -ne O ]] || [[ ${PlayingBoard[$i]} -ne X ]]
+				then
+					echo "same variable found in row "$i
+				fi
+			done
+		else
+			row=$(( $row+2 ))
+		fi
+	done
+}
+
+function coloumnCanWin(){
+	local coloumn=0;
+	for (( count=1; count<=3; count++ ))
+	do
+		coloumn=$(( $coloumn+1 ))
+		if [[ ${PlayingBoard[$coloumn]} == ${PlayingBoard[$coloumn+3]} ]] || [[ ${PlayingBoard[$coloumn+3]} == ${PlayingBoard[$coloumn+6]} ]] || [[ ${PlayingBoard[$coloumn+6]} == ${PlayingBoard[$coloumn]} ]] 
+		then
+			for (( i=1; i<=3; i++ ))
+			do
+				if [[ ${PlayingBoard[$coloumn]} -ne O ]] || [[ ${PlayingBoard[$coloumn]} -ne X ]]
+				then
+					echo "Same variable found in col "$coloumn
+				fi
+				coloumn=$(( $coloumn+3 ))
+			done
+		fi
+	done
+}
+
+function diagonalCanWin(){
+	local diagCount=1;
+	local count=1;
+	if [[ ${PlayingBoard[$diagCount]} == ${PlayingBoard[$diagCount+4]} ]] || [[ ${PlayingBoard[$diagCount+4]} == ${PlayingBoard[$diagCount+8]} ]] || [[ ${PlayingBoard[$diagCount+8]} == ${PlayingBoard[$diagCount]} ]]
+	then
+		echo "In diagonal"
+		for (( i=1; i<=3; i++ ))
+		do
+			if [[ ${PlayingBoard[$diagCount]} -ne O ]] || [[ ${PlayingBoard[$diagCount]} -ne X ]]
+			then
+				echo "Same vertical found in diagonal " $diagCount
+			fi
+			diagCount=$(( $diagCount+4 ))
+		done
+	elif [[ ${PlayingBoard[$count+2]} == ${PlayingBoard[$count+4]} ]] || [[ ${PlayingBoard[$count+4]} == ${PlayingBoard[$count+6]} ]] || [[ ${PlayingBoard[$count+6]} == ${PlayingBoard[$count+2]} ]]
+	then
+		echo "Tn diagonal space"
+		for (( i=1; i<=3; i++ ))
+		do
+			count=$(( $count+2 ))
+			if [[ ${PlayingBoard[$count]} -ne O ]] || [[ ${PlayingBoard[$count]} -ne X ]]
+			then
+				echo "Same veriable found in diagonal "$count
+			fi
+		done
+	fi
+}
+
 displayBoard
 while [ true ]
 do
-	read -p "Enter your choice" replace
+	moveCount=$(( $moveCount+2 ))
+	read -p "Enter your choice: " replace
 	findReplace $replace
 	displayBoard
+	rowCanWin
+	coloumnCanWin
+	diagonalCanWin
+	#assignAndReplace
 	winner=$( winnerCheck )
 	if [ $winner == true ]
 	then
+		echo "PLAYER WON"
 		break;
 	fi
+
+	if [ $moveCount -gt 9 ]
+	then 
+		echo "TIE"
+		break;
+	fi
+
+	# read -p "Enter your choice" replace
+	# findReplaceCom $replace
+	# displayBoard
+	# #assignAndReplace
+	# winner=$( winnerCheck )
+	# if [ $winner == true ]
+	# then
+	# 	echo "COMPUTER WON"
+	# 	break;
+	# fi
 done
